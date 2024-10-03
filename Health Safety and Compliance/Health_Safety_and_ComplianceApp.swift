6a48ecb5d53cb6c10633505125365884b2520084
Anthony <anthonybacon916@gmail.com>
2024-10-03 10:02:12 +0100

    Initial Commit

---
+++ Health Safety and Compliance/Health_Safety_and_ComplianceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Safety_and_ComplianceApp.swift
+//  Health Safety and Compliance
+//
+//  Created by Anthony Bacon on 03/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Health_Safety_and_ComplianceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Health Safety and ComplianceTests/Health_Safety_and_ComplianceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Safety_and_ComplianceTests.swift
+//  Health Safety and ComplianceTests
+//
+//  Created by Anthony Bacon on 03/10/2024.
+//
+
+import Testing
+@testable import Health_Safety_and_Compliance
+
+struct Health_Safety_and_ComplianceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
